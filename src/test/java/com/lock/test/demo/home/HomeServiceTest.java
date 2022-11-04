9d80349c689dc5044c1ad0993ab807d3a6006498
junsj119 <wjstjddud119@naver.com>
2022-11-04 16:01:29 +0900

    no message

---
+++ src/test/java/com/lock/test/demo/home/HomeServiceTest.java
@@ -0,0 +1,47 @@
+package com.lock.test.demo.home;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class HumanServiceTest {
+    @Autowired
+    HomeService homeService;
+
+    @Test
+    @DisplayName("돈 줄여보기(멀티 스레드) 테스트")
+    void decreaseMoneyForMultiThreadTest() throws InterruptedException {
+        AtomicInteger successCount = new AtomicInteger();
+        int COUNT = 100;
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(COUNT);
+
+        for (int i = 0; i < COUNT; i++) {
+            service.execute(() -> {
+                try {
+                    homeService.decreaseMoney("조재영", 1000);
+                    successCount.getAndIncrement();
+                    System.out.println("성공");
+                } catch (ObjectOptimisticLockingFailureException oe) {
+                    System.out.println("충돌감지");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                latch.countDown();
+            });
+        }
+        latch.await();
+
+        assertThat(successCount.get()).isEqualTo(10);
+    }
+}
